67150bccdd541d6ee9528a17f383e6217fd3cf1b
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-01-28 20:43:52 +0100

    options component

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -6,8 +6,8 @@ import ProfileImage from './Components/Profile_Image';
 
 const Profile = () => {
     return (
-        <div className='flex items-center justify-center text-center w-full h-[50vh] min-h-[400px] max-h-[500px]'>
-            <div className='flex-col padd:flex-row flex items-center gap-4 w-full justify-center h-[80%]'>
+        <div className='flex items-center justify-center text-center w-full h-[50vh]'>
+            <div className='flex-col padd:flex-row flex items-center gap-4 w-full justify-center h-[80%] min-h-[400px] max-h-[500px]'>
                 <div className='w-full h-full flex flex-col items-center justify-between'>
                     <ProfileHeader />
                     <ProfileTypwriter />
