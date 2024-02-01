416c58ba4f39f802d1d71b6c6380c03e81d473d0
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-02-01 23:28:42 +0100

    nav adjusted

--- src/Components/Layouts/Navigation/components/Burger/Burger.jsx
+++ src/Components/Layouts/Navigation/components/Burger/Burger.jsx
@@ -3,7 +3,7 @@ import BurgerLine from "./BurgerLine";
 const Burger = ({menuHandler, open}) => {
     return (
         <>        
-            <div className="flex w-full md:hidden items-end flex-col relative cursor-pointer">
+            <div className="flex w-full lg:hidden items-end flex-col relative cursor-pointer">
                 <div className={`${open ? "h-0" : "h-5"} text-2xl relative w-6 z-50 transition-all duration-200`} onClick={menuHandler}>
                     <BurgerLine custom={`top-0 ${open ? '-rotate-45' : ""}`} />
                     <BurgerLine custom={`top-1/2 ${open ? 'opacity-0 translate-x-4' : "opacity-100"}`} />
