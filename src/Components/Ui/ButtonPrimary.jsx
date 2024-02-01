3cbb4b3341882c6a338232a6883362fcb16abb38
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-02-01 15:04:50 +0100

    contact firebase and emailjs

--- src/Components/Ui/ButtonPrimary.jsx
+++ src/Components/Ui/ButtonPrimary.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const ButtonPrimary = ({children, button}) => {
+const ButtonPrimary = ({children, button , onClick}) => {
+    let handleClick = () => {
+        if (onClick) {
+            onClick();
+        }
+    };
+
     return (
-        <button button={button} className={`rounded-lg text-whiteish bolder border-2 border-caribbean_600 py-4 px-8 max-w-[290px] w-full cursor-pointer text-lg font-bold text-md bg-caribbean_600 hover:bg-caribbean_700 transition duration-500 hover:shadow-middle transtion  hover:shadow-caribbean_500`}>
+        <button {...button} onClick={handleClick} className={`rounded-lg text-whiteish bolder border-2 border-caribbean_600 py-4 px-8 max-w-[290px] w-full cursor-pointer text-lg font-bold text-md bg-caribbean_600 hover:bg-caribbean_700 transition duration-500 hover:shadow-middle transtion  hover:shadow-caribbean_500`}>
             {children}
         </button>
     );
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,50 +1,78 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Heading from '../../Components/Heading/Heading';
 import ButtonPrimary from '../../Components/Ui/ButtonPrimary';
 import { LuSend } from "react-icons/lu";
 
+
 const Contact = () => {
+    let form = useRef();
+    let [name, setName] = useState("")
+    let [email, setEmail] = useState("")
+    let [message, setMessage] = useState("")
+    let [error, setError] = useState(false);
+    let [succes, setSucces] = useState(false);
 
-    const form = useRef();
+    let nameHandler = (e) => {
+        setName(e.target.value)
+    }
+    let emailHandler = (e) => {
+        setEmail(e.target.value)
+    }
+    let messageHandler = (e) => {
+        setMessage(e.target.value)
+    }
 
-    const sendEmail = (e) => {
+    let formHandler = async (e) => {
         e.preventDefault();
-    
-        emailjs.sendForm('service_bx2imh9', 'template_qtj543b', form.current, 'mcrKYJtLl9odWjGFq')
 
-        e.target.reset();
-    };
+        setError(false);
+        
+        let data ={
+            name, 
+            email,
+            message
+        }
+
+        if (name === null || name === "" && email === null || email === "" && message === null || message === "" ) {
+
+            setError(true)
+            return;
+            
+        } else {
+            await fetch("https://portfolio-ba9ea-default-rtdb.europe-west1.firebasedatabase.app/messages.json", { method: "POST", headers: {"Content-Type" : "application/json"}, body: JSON.stringify(data)})
+    
+            await emailjs.sendForm('service_bx2imh9', 'template_qtj543b', form.current, 'mcrKYJtLl9odWjGFq')
+    
+            e.target.reset();
+        }
+        
+    }
 
     return (
-        <div className="dm-32 w-full h-screen flex justify-center items-center p-4">
-            <div className='w-fit justify-center flex items-center'>
+        <div className="m-32 w-full h-screen flex justify-center flex-col items-center p-4">
+            <div className='w-1/2 justify-center flex items-center'>
                 <Heading heading="Contact" />
             </div>
-            <div className='w-1/2 flex h-[80%] justify-center items-center'>
-                <form className='flex flex-col gap-4 w-full' ref={form} onSubmit={sendEmail}>
+            <div className='back-form w-1/2 flex justify-center items-center'>
+                <form className='flex flex-col gap-8 w-full' onSubmit={formHandler} ref={form}>
                     <div className='flex gap-8'>
-                        <div className='flex flex-1 flex-col'>
-                            <label htmlFor="name" className=''>Name</label>
-                            <input type="text" name="name" className=' bg-transparent border-2 border-nobel_100 rounded-md p-2 outline-none'/>
+                        <div className='flex flex-col'>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" onChange={nameHandler} value={name} className={`bg-transparent border-2 ${!error ? "border-nobel_100" : "border-red-400" } rounded-md p-2 outline-none `}/>
                         </div>
-                        <div className='flex flex-1 flex-col'>
-                            <label htmlFor="email" className=''>Email</label>
-                            <input type="email" name="email" className='bg-transparent border-2 border-nobel_100 rounded-md p-2 outline-none'/>
+                        <div className='flex flex-col'>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" onChange={emailHandler} value={email} className=' bg-transparent border-2 border-nobel_100 rounded-md p-2 outline-none' />
                         </div>
                     </div>
                     <div className='flex flex-col'>
                         <label htmlFor="message">Message</label>
-                        <textarea type="text" name="message" className='bg-transparent border-2 border-nobel_100 rounded-md p-2 outline-none'/>
+                        <textarea type="text" name="message" onChange={messageHandler} value={message} className=' bg-transparent border-2 border-nobel_100 rounded-md p-2 outline-none' />
                     </div>
-                    <div className='w-full flex justify-center items-center'>
-                        <ButtonPrimary type='submit'>
-                            <div className='flex justify-center items-center'>
-                                <h2 className='flex-1'>
-                                    Send
-                                </h2>
-                                <LuSend />
-                            </div>
+                    <div className='send-btn w-full flex justify-center items-center'>
+                        <ButtonPrimary button={{ type: 'submit' }}>
+                            send
                         </ButtonPrimary>
                     </div>
 
@@ -52,6 +80,7 @@ const Contact = () => {
             </div>
         </div>
     );
+
 }
 
 export default Contact;
