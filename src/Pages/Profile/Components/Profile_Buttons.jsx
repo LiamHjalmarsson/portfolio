781507126fb5082da6cfa644aa07a6e0b34cc6de
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-02-02 12:13:38 +0100

    links and buttons conected

--- src/Pages/Profile/Components/Profile_Buttons.jsx
+++ src/Pages/Profile/Components/Profile_Buttons.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import ButtonPrimary from '../../../Components/Ui/ButtonPrimary';
 import ButtonSecondary from '../../../Components/Ui/ButtonSubmary';
-import { ScrollLink } from 'react-scroll';
+import { Link, ScrollLink } from 'react-scroll';
 
 const ProfileButtons = () => {
     return (
         <div className='w-full items-center justify-center flex gap-8 md:flex-row flex-col max-w-[400px] md:max-w-[600px]'>
-            <ButtonPrimary>
-                Contact Me
-            </ButtonPrimary>
+            <Link to="contact" smooth="true" offset={0} duration={500} className='max-w-[200px] w-full'>
+                <ButtonPrimary>
+                    Contact Me
+                </ButtonPrimary>
+            </Link>
 
-            {/* <a href="liam.pdf" download="liam.pdf" className=' w-full'> */}
-            <ButtonSecondary>
-                Get Resume
-            </ButtonSecondary>
-            {/* </a> */}
+            <a href="/liam.pdf" download="liam.pdf" className='w-full max-w-[200px]'>
+                <ButtonSecondary>
+                    Get Resume
+                </ButtonSecondary>
+            </a>
         </div>
     );
 }
