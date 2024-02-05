3a2d268702c3fe000959f7e65c8921655ec4c1b9
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-02-05 09:09:14 +0100

    height profile

--- src/Components/Layouts/Section/Section.jsx
+++ src/Components/Layouts/Section/Section.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section = ({id, children}) => {
     return (
-        <div className={`flex items-end laptop:items-center justify-center flex-col text-center w-full ${id === "home" ? "min-h-[280px] lg:min-h-[100vh]" : "min-h-screen"} px-5 md:px-10 lg:px-20 py-8`} id={id}>  
+        <div className={`flex items-end laptop:items-center justify-center flex-col text-center w-full ${id === "home" ? "min-h-[900px] lg:min-h-[100vh]" : "min-h-screen"} px-5 md:px-10 lg:px-20 py-8`} id={id}>  
             {children}
         </div>
     );
