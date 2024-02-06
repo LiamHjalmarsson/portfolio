69a61312980730dffac8c9380ea7fadba03c33aa
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-02-06 10:09:02 +0100

    Contact messages started
    Clean up and adjustments needed to be done

--- src/Pages/Contact/components/error.jsx
+++ src/Pages/Contact/components/error.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Error = () => {
+const Error = ({ showError }) => {
     return (
-        <div>
-            
+        <div className={` border-red-500 border shadow-middle shadow-red-500 p-4 absolute top-[20%] right-0 text-red-500 transition-transform duration-500 delay-200`}>
+            Please fill in all the fields.
         </div>
     );
 }
